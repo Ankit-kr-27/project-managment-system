9c64a8e6d1fa708a3061f04cd3f65cdd81d12cb6
Ankit kumar <kr.ankit2783@gmail.com>
2026-01-12 19:40:31 +0530

    made save hookes for user schema and done password hashing

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose,{ Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new Schema(
     {
@@ -58,6 +59,17 @@ const userSchema = new Schema(
     timestamps: true,
     },
 );
+
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next ()
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
 
 
 
