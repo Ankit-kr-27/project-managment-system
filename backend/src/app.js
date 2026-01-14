aa762f2880383b33e1247dac33661cc17f81f367
Ankit kumar <kr.ankit2783@gmail.com>
2026-01-14 20:23:17 +0530

    implemented login and logout feature

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 
 
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-// app.use(cookieParser());
+app.use(cookieParser());
 
 // cors configurations
 app.use(
